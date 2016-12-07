3bb0e8fb8bca1e2d5e7d4833bf251ae6e81b3d03
avelina <kim_avelina_sun_youn@student.smc.edu>
2016-12-06 19:47:54 -0800

    Create form to customize list items.

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
 
 $(document).ready(function() {
   function removeItem() {
-    console.log("remove clicked");
     $(this).parent().remove();
   }
 
-  $("#add-item-btn").click(function() {
+  $("#submit-btn").click(function(event) {
+    event.preventDefault();
+    $("form [name='item-name']").val(); 
     var newButton = $("<button class='remove-item-btn'>remove</button>");
     newButton.click(removeItem);
-    var newItem = $("<li>Added item</li>");
+    var str =  $("form [name='item-name']").val(); 
+    var newItem = $("<li>" + str + "</li>");
     newItem.append(newButton);
     $("ul").append(newItem);
   });
