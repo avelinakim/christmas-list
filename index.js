19ecfebba9d1af9b862115795904fb12a05355ae
avelina <kim_avelina_sun_youn@student.smc.edu>
2016-12-06 21:39:42 -0800

    Initialize firebase

--- index.js
+++ index.js
@@ -1,27 +1,40 @@
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyAkL2aLLV6UOLbYguuEUxyQ36GM19h9WZM",
+  authDomain: "christmas-list-aedc4.firebaseapp.com",
+  databaseURL: "https://christmas-list-aedc4.firebaseio.com",
+  storageBucket: "christmas-list-aedc4.appspot.com",
+  messagingSenderId: "872134012302"
+};
+firebase.initializeApp(config);
+var database = firebase.database(); 
 
 $(document).ready(function() {
-  //removes what removeItem is triggered on 
+  // removes what removeItem is triggered on 
   function removeItem() {
     $(this).parent().remove();
   }
 
-  //create list item and add to list
+  // create list item and add to list
   function createItem(name, notes, link) {
-    var newItem = $("<li>" + name + " </li>");
+    var JSItem = { name: name, notes: notes, link: link };
+    database.ref('list').push(JSItem); 
+    var newItem = $("<li></li>");
     var notesSpan = $("<span>" + notes + "</span>");
     var itemLink = $("<a href='" + link + "'> Link </a>"); 
     var newButton = $("<button class='remove-item-btn'>remove</button>");
     newButton.click(removeItem);
     
-    //append data to item and add to list 
+    // append data to item and add to list 
+    newItem.append(newButton);
+    newItem.append(" " + name + " ");
     newItem.append(notesSpan); 
     if (link != "")
       newItem.append(itemLink);
-    newItem.append(newButton);
     $("ul").append(newItem);
   }
 
-  //collect data from form and calls createItem
+  // collect data from form and calls createItem
   $("#submit-btn").click(function(event) {
     event.preventDefault();
    
