3b7def679c0cf0d54adc0b8103f4591520290ced
avelina <kim_avelina_sun_youn@student.smc.edu>
2016-12-06 20:47:09 -0800

    Form creates a list item with name, description, and optional link.

--- index.js
+++ index.js
@@ -1,17 +1,33 @@
 
 $(document).ready(function() {
+  //removes what removeItem is triggered on 
   function removeItem() {
     $(this).parent().remove();
   }
 
-  $("#submit-btn").click(function(event) {
-    event.preventDefault();
-    $("form [name='item-name']").val(); 
+  //create list item and add to list
+  function createItem(name, notes, link) {
+    var newItem = $("<li>" + name + " </li>");
+    var notesSpan = $("<span>" + notes + "</span>");
+    var itemLink = $("<a href='" + link + "'> Link </a>"); 
     var newButton = $("<button class='remove-item-btn'>remove</button>");
     newButton.click(removeItem);
-    var str =  $("form [name='item-name']").val(); 
-    var newItem = $("<li>" + str + "</li>");
+    
+    //append data to item and add to list 
+    newItem.append(notesSpan); 
+    if (link != "")
+      newItem.append(itemLink);
     newItem.append(newButton);
     $("ul").append(newItem);
+  }
+
+  //collect data from form and calls createItem
+  $("#submit-btn").click(function(event) {
+    event.preventDefault();
+   
+    var itemName =  $("form [name='item-name']").val(); 
+    var notes = $("form [name='notes']").val();
+    var link = $("form [name='link']").val(); 
+    createItem(itemName, notes, link);
   });
 });
